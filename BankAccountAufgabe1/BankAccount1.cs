882aa059d6787e7159903da30c6203683731cf08
YoelArcos <Yoel.arcoslopez@hotmail.com>
2024-05-10 12:17:23 +0200

    Projektdateien hinzufügen.

---
+++ BankAccountAufgabe1/BankAccount1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccountAufgabe1
+{
+    public class BankAccount1
+    {
+        public  string Number { get; private set; }
+        public  string Owner { get; private set; }
+        public float Balance { get; private set; }
+
+        public BankAccount1(string number, string owner, float balance)
+        {
+            this.Number= number;
+            this.Owner= owner;
+            this.Balance= balance;
+        }
+        
+        public void MakeDeposit(float amount)
+        {
+            Balance += amount;
+        }
+
+        public void MakeWithdrawal(float amount) 
+        {
+            if(amount <= Balance)
+            {
+                Balance -= amount;
+            }
+            else
+            {
+                Console.WriteLine("Unzureichender Saldo!\n");
+            }
+        }
+
+        public void PrintBalance()
+        {
+            Console.WriteLine($"Kontostand von {Owner}: {Balance}\n");
+        }
+            
+        }
+    }
+
---
+++ BankAccountAufgabe1/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Buffers;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using BankAccountAufgabe1;
+
+namespace BankAccountAufgabe1
+{
+    internal class Programm
+    {
+        public static void Main(string[] args)
+        {
+            
+            
+                
+            Console.WriteLine("Bitte gebe deinen Namen ein:");
+            string ownerName = Console.ReadLine();
+            
+
+            Console.WriteLine("Gebe bitte deine 4-stellige Kartennummer ein:");
+            string ownerNumber= Console.ReadLine();
+
+            Console.WriteLine("Gebe bitte deinen Kontostand ein:");
+            float balance = float.Parse(Console.ReadLine());
+
+            BankAccount1 account = new BankAccount1(ownerNumber, ownerName, balance );
+
+
+            while (true)
+            {
+                Console.WriteLine("Bitte wählen Sie eine Aktion aus:");
+                Console.WriteLine("1.Kontostand anzeigen");
+                Console.WriteLine("2.Einzahlung machen");
+                Console.WriteLine("3.Auszahlung machen");
+                Console.WriteLine("4.Beenden");
+
+                string input = Console.ReadLine();
+
+                if (input == "1")
+                {
+                    account.PrintBalance();
+
+                }
+                else if (input == "2")
+                {
+                    Console.WriteLine("Bitte geben sie den Betrag der eingezahlt werden soll.\n");
+                    float depositAmount = float.Parse(Console.ReadLine());
+                    account.MakeDeposit(depositAmount);
+
+                }
+                else if (input == "3")
+                {
+                    Console.WriteLine("Bitte geben sie den Betrag ein den sie abheben möchten.\n");
+                    float withdrawalAmount = float.Parse(Console.ReadLine()); 
+                    account.MakeWithdrawal(withdrawalAmount);
+                }
+                else if (input == "4")
+                {
+
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("ungültige Auswahl, erneut versuchen.\n");
+                }
+            }
+
+
+        }
+    }
+}
